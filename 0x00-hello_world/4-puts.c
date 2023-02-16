a9611cd7f46dfb6a95eaa1dfeb81e833d2fd59a2
ConquerorCletus <smartchibuike2019@gmail.com>
2023-02-16 02:54:14 -0800

    4-puts task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+/*This program displays with the outlut with the put function instead of printf*/
+int main(void){
+     puts("\"Programming is like building a multilingual puzzle");
+	      return(0);
+}       
