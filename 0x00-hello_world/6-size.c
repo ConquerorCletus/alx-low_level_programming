57aee065189cfd7f0b14275492a26c73cb0540ee
ConquerorCletus <smartchibuike2019@gmail.com>
2023-02-16 11:08:39 -0800

    size task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * main - This program prints out the size of various types on the computer
+ * main - this program prints out the size of primitive characters
  * Return: 0 after compilation
  */
 int main(void)
 {
 	printf("size of a char:%ld byte(s)\n", sizeof(char));
 	printf("size of an int:%ld byte(s)\n", sizeof(int));
-	printf("size of long int:%ld byte(s)\n", sizeof(long int));
-	printf("size of long long int:%ld byte(s)\n", sizeof(long long int));
-	printf("size of float: %ld byte(s)\n", sizeof(float));
+	printf("size of a long int:%ld byte(s)\n", sizeof(long int));
+	printf("size of a long long int:%ld byte(s)\n", sizeof(long long int));
+	printf("size of a float:%ld byte(s)\n", sizeof(float));
 	return (0);
 }
