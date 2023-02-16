12873494554c87c487e98fed23733abfd648e07c
ConquerorCletus <smartchibuike2019@gmail.com>
2023-02-16 07:41:41 -0800

    task size.c done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,15 @@
  */
 int main(void)
 {
-	printf("size of a char:%lu byte(s)\n", sizeof(char));
-	printf("size of an int:%lu byte(s)\n", sizeof(int));
-	printf("size of long int:%lu byte(s)\n", sizeof(long int));
-	printf("size of long long int:%lu byte(s)\n", sizeof(long long int));
-	printf("size of float: %lu byte(s)\n", sizeof(float));
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	printf("size of a char:%lu byte(s)\n", sizeof(a));
+	printf("size of an int:%lu byte(s)\n", sizeof(b));
+	printf("size of long int:%lu byte(s)\n", sizeof(c));
+	printf("size of long long int:%lu byte(s)\n", sizeof(d));
+	printf("size of float: %lu byte(s)\n", sizeof(e));
 	return (0);
 }
