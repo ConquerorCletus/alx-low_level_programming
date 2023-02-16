d5c3218b66b44780d63af98fa192b54b531b49d3
ConquerorCletus <smartchibuike2019@gmail.com>
2023-02-16 03:25:08 -0800

    printf task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/*I am going to use the printf function to display a particular output*/
+
+int main(void) {
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
+
