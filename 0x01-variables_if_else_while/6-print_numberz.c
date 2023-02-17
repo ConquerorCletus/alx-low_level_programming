d15502a6b88274827c5872fb184b0ee5572ec00e
ConquerorCletus <smartchibuike2019@gmail.com>
2023-02-17 13:43:21 -0800

    6-print_numberz task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - this program display single digits of base 10
+ * Date: 17-02-2023
+ * Return: 0 after compilation
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+		putchar((char)a);
+	putchar('\n');
+	return (0);
+}
