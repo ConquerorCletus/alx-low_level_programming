98a62828b8d1887fe9d7dd7fd9913f384718a933
ConquerorCletus <smartchibuike2019@gmail.com>
2023-02-17 08:01:09 -0800

    task has started

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - This program would make use of if and else if.
+ * Return: 0 aftr success compilation
+ * date: 17-02-2023
+ */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX/2;
+	if (n > 0){
+		printf("%d is positive \n",n);
+	}
