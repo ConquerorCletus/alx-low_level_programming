06808bb46e56bd958c15eda2703bb3f342b788ea
ConquerorCletus <smartchibuike2019@gmail.com>
2023-02-17 13:36:05 -0800

    5-print_numbers task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+/**
+ *main - program count from 0 to 9 by increment
+ * Date:17-02-2023
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		printf("%d", a);
+	printf("\n");
+	return (0);
+}
