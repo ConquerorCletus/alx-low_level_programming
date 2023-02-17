c951b5c69b92a40dd40bfe9e65a1992a6fdc572a
ConquerorCletus <smartchibuike2019@gmail.com>
2023-02-17 12:00:25 -0800

    3-print_alphabets task done

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - This program would print a to z in upper and lowercase.
+ * date: 17-02-2023
+ * Return: 0 after compilation
+ */
+int main(void)
+{
+	for (char az = 'a'; az <= 'z'; az++)
+{
+	putchar(az);
+}
+for (char az = 'A'; az <= 'Z'; az++)
+{
+	putchar(az);
+}
+putchar('\n');
+return (0);
+}
+
