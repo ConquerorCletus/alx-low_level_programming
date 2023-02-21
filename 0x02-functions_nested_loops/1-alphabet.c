5fd9eb833cbae0cbf0cefa11cbd264e3d1f8e23b
ConquerorCletus <smartchibuike2019@gmail.com>
2023-02-21 05:37:54 -0800

    1-alphabet.c task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - prints all alphabet
+ */
+void print_alphabet(void)
+{
+	char az;
+
+	for (az = 'a'; az <= 'z'; az++)
+		_putchar(az);
+	_putchar('\n');
+}
