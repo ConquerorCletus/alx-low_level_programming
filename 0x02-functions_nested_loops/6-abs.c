019a453562f98f7da525fff194c29043cc8ab6dc
ConquerorCletus <smartchibuike2019@gmail.com>
2023-02-21 07:12:44 -0800

    6-abs task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - print the absolute value of an integer
+ * @num: the number checked
+ *
+ * Return: the absolute value of num
+ */
+int _abs(int num)
+{
+	if (num >= 0)
+		return (num);
+	else
+		return (-num);
+}
