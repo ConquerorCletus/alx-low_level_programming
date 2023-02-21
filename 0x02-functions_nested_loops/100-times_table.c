fe6c4e4091018ae058d304a7ae4ec88b828f61f1
ConquerorCletus <smartchibuike2019@gmail.com>
2023-02-21 13:31:40 -0800

    timestable task redo

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,7 +25,7 @@ void print_times_table(int n)
 				if (c >= 100)
 				{
 					_putchar((c / 100) + '0');
-					_putchar((a / 10) & 10 + '0');
+					_putchar((a / 10) % 10 + '0');
 							}
 else if (c <= 99 && c >= 10)
 {
