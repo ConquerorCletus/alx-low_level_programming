d8e559b4c2cfae000428a5d73f5843ce93e73651
ConquerorCletus <smartchibuike2019@gmail.com>
2023-02-21 04:15:35 -0800

    0-putchar.c task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+/**
+ * main - program prints _putchar with the _putchar function call.
+ * Date: 21-02-2023
+ * Return: 0 after compilation
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
+
