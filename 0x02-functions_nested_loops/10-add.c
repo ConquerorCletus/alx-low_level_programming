2447ac95c788dd5785371fd1477ce803936eb123
ConquerorCletus <smartchibuike2019@gmail.com>
2023-02-21 07:42:54 -0800

    10-add.c task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - add two integer value and return result
+ * @n1: first integer
+ * @n2: second integer
+ *
+ * Return: sum of n1 + n2
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
