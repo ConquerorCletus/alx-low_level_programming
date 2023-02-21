0c89631092736593f0a4d4052cda1d4c905ef29a
ConquerorCletus <smartchibuike2019@gmail.com>
2023-02-21 08:49:50 -0800

    9-times_table.c task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,10 +8,14 @@ void times_table(void)
 	int a, b, c;
 
 	for (a = 0; a < 10 ; a++)
+	{
 	for (b = 0; b < 10; b++)
+	{
 	c = b * a;
 if (b == 0)
+{
 _putchar(c + '0');
+}
 	if (c < 10 && b != 0)
 	{
 		_putchar(',');
@@ -26,5 +30,7 @@ else if (c >= 10)
 	_putchar((c / 10) + '0');
 	_putchar((c % 10) + '0');
 }
+}
 _putchar('\n');
+}
 }
