0592f9da106e2feeb6116aa6b668616add6b221f
ConquerorCletus <smartchibuike2019@gmail.com>
2023-02-23 10:51:47 -0800

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,7 +13,11 @@ void print_square(int size)
 		_putchar('\n');
 
 	for (a = 0; a < size; a++)
-		_putchar('\n');
-	for (b = 0; b < size; b++)
+	{
+	for (b = 0; b < (size); b++)
+	{
 		_putchar('#');
+	}
+	_putchar('\n');
+	}
 }
