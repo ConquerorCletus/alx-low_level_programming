b3ebb04fe373b9a84ab5200a3922c90f817aaf32
ConquerorCletus <smartchibuike2019@gmail.com>
2023-02-23 10:46:34 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_square - this function primt out squares
+ * @size: parameter used
+ * Return:void
+ * Date:23-02-2023
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (a = 0; a < size; a++)
+		_putchar('\n');
+	for (b = 0; b < (size); b++)
+		_putchar('#');
+}
