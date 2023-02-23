72392a259d74a040f814ee414bdc941a7bb691bc
ConquerorCletus <smartchibuike2019@gmail.com>
2023-02-23 09:48:07 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,15 @@ void more_numbers(void)
 	int a, b;
 
 	for (a = 1; a <= 10; a++)
+	{
 		for (b = 0; b <= 14; b++)
+		{
 			if (b >= 10)
 			{
 				_putchar('1');
 				_putchar(b % 10 + '0');
 			}
+}
+}
 _putchar('\n');
 }
