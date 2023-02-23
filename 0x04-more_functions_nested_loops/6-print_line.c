cc6c6d2acd44b4104de21a151cb0b3c13f0e10ce
ConquerorCletus <smartchibuike2019@gmail.com>
2023-02-23 10:08:46 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,11 +7,14 @@
  */
 void print_line(int n)
 {
-	int line = 0;
+	int line = 1;
 
-	while (line < n && n > 0)
-		_putchar('_');
-	line++;
+	for (line = 1; line <= n; line++)
+	{
+		if (n <= 0)
+			_putchar('\n');
+		else
+			_putchar('_');
+	}
 	_putchar('\n');
 }
-
