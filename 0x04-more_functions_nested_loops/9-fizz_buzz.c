bf1b5624a4aa856c98516693095b9574c72a6688
ConquerorCletus <smartchibuike2019@gmail.com>
2023-02-23 12:18:38 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,15 +11,17 @@ int main(void)
 	for (a = 1; a <= 100; a++)
 	{
 		if (a % 3 == 0 && a % 5 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (a % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (a % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
-			printf("%i ", a);
+			printf("%i", a);
+		if (a < 100)
+			printf(" ");
 	}
-	putchar('\n');
+	printf("\n");
 	return (0);
 }
 
