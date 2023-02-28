c183aea92922e3db40d7c6956911a550cd258f59
ConquerorCletus <smartchibuike2019@gmail.com>
2023-02-27 16:35:34 -0800

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - This program generates random values as password.
+ * Return:0
+ * Date:27-02-2023
+ */
+int main(void)
+{
+	int code;
+
+	srand(time(0));
+	code = rand();
+	printf("%d\n", code);
+	return (0);
+}
