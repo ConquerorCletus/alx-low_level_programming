ed22daadae47e16861ae2ecb02b958ddca5f0e47
ConquerorCletus <smartchibuike2019@gmail.com>
2023-03-01 09:34:59 -0800

    1-strncat task

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncat - This function concatenates 2 strings with n bytes.
+ * @dest: Destination of copied string
+ * @src: source of string
+ * @n: number of bytes
+ * Return: dest
+ * Date: 1-03-2023
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, b;
+
+	a = 0;
+	while (dest[a])
+		a++;
+
+	for (b = 0; b < n && src[b] != '\0'; b++)
+		dest[a + b] = src[b];
+	dest[a + b] = '\0';
+	return (dest);
+}
