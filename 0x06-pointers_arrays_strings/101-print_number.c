2360b00405c630e5a50dfef74b37b25d66230feb
ConquerorCletus <smartchibuike2019@gmail.com>
2023-03-02 05:47:01 -0800

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_number - This function prints an integer.
+ * @n: The integer parameter.
+ * Return: integer
+ * Date: 2-03-2023
+ */
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
+}
