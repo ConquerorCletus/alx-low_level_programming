6a98497af9de2b2e33f6447539784dc2a032b56f
ConquerorCletus <smartchibuike2019@gmail.com>
2023-03-02 05:12:46 -0800

    100-rot13 task

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * rot13 - This Funection encodes a string using rot13
+ * @s:The string to encode.
+ * Return: dest.
+ * Date: 2-03-2023
+ */
+
+char *rot13(char *s)
+{
+	int count = 0, i;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (*(s + count) == alphabet[i])
+			{
+				*(s + count) = rot13[i];
+				break;
+			}
+		}
+		count++;
+	}
+
+	return (s);
+}
