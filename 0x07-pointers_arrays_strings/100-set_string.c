5bb705d429b4a5a1bd7ce4cb5cbbfdf7ce824b89
ConquerorCletus <smartchibuike2019@gmail.com>
2023-03-06 07:06:04 -0800

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - This function sets the value of a pointer to a char
+ * @s: source adress.
+ * @to: target adress.
+ * Date: 7-03-2023
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
