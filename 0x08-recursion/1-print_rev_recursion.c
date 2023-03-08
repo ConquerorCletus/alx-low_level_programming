bae4c5af6f1806cbd9a9b3d079ebab554201ba6e
ConquerorCletus <smartchibuike2019@gmail.com>
2023-03-08 09:30:04 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- * _strlen_recursion - a function that returns length of a string
- * @s: pointer to s
- * Return: length of s
- * Date:8-03-2023
+ * _print_rev_recursion - function that prints a string in reverse.
+ * @s: string to be printed.
+ * Return:void
+ * Date: 8-03-2023
  */
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-		return (0);
-	s++;
-	return (_strlen_recursion(s) + 1);
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
