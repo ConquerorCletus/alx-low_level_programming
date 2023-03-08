15601ddb78d2e5c6ec3168039a08c4452e0da0db
ConquerorCletus <smartchibuike2019@gmail.com>
2023-03-08 11:46:40 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,32 +1,31 @@
 #include "main.h"
- /**
-  * _sqrt_recursion - This function returns the square root of n
-  * Date:8-03-2023
-  * @n: input value
-  * Return: If the number has a natural square root - the square root.
- If the number does not have a natural square root - -1 
-  */ 
-  
-int _sqrt_recursion(int n)
-{
-	if (n == 0 || n == 1)
-		return (n);
-	return (_sqrt(0, n));
-}
-  
 /**
-* _sqrt - Finds the square root of a number
-* @n: The input value.
-* @x: The root to be tested
-* Return: If the number has a natural square root - the square root
- If the number does not have a natural square root - -1.
-  */ 
-  
-int _sqrt(int n, int x)
+ * _sqrt_recursion - Returns the square root of n
+ * @n: input value to find square root of
+ * Return: If the number has a natural square root - the square root.
+ *         If the number does not have a natural square root - -1.
+ */
+
+int _sqrt_recursion(int n)
 {
-	if (n > x / 2)
-		return (-1);
-	else if (n * n == x)
-		return (n);
-	return (_sqrt(n + 1, x));
+	if (n == 0 || n == 1)
+		return (n);
+	return (_sqrt(0, n));
+}
+
+/**
+ * _sqrt - Finds the square root of a number
+ * @n: The number to find the square root of.
+ * @x: The root to be tested.
+ * Return: If the number has a natural square root - the square root.
+ *         If the number does not have a natural square root - -1.
+ */
+
+int _sqrt(int n, int x)
+{
+	if (n > x / 2)
+		return (-1);
+	else if (n * n == x)
+		return (n);
+	return (_sqrt(n + 1, x));
 }
