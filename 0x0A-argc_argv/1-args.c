a932bb815c6483cf692d14f968a7a31275c3384e
ConquerorCletus <smartchibuike2019@gmail.com>
2023-03-10 09:50:27 -0800

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - this program prints number of arguments
+ * @argc: argument count parameter
+ * @argv: argument vector
+ * Return: 0
+ * Date:10-03-2023
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+		printf("%d\n", argc - 1);
+	return (0);
+}
