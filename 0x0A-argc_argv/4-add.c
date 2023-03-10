3849c7bfdd71142c0a9b4c37d728a40676312ec3
ConquerorCletus <smartchibuike2019@gmail.com>
2023-03-10 14:32:17 -0800

    add task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*#include <ctype.h>*/
 /**
  * main - program that adds integers
  * @argc: argument constant parameter
@@ -9,14 +10,18 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int i, j, sum = 0;
 
+	for (i = 1; i < argc; i++)
 	{
-		printf("Error\n");
-		return (1);
-	}	
-	for (i = 0; i < argc; i++)
-	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+	}
 		sum += atoi(argv[i]);
 	}
 		printf("%d\n", sum);
