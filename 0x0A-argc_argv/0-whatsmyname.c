d5b309ebf86964ee8602f5d60ffa0f057c34c9ff
ConquerorCletus <smartchibuike2019@gmail.com>
2023-03-10 09:14:35 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that prints its name
+ * @argc: argument count parameter
+ * @argv: pointer to argument vector
+ * Return: 0
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	int i = 0;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+# 0x0A - ARGC and ARGV
