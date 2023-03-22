cd3df416c99760d18302abf36c06e11685cfdbed
ConquerorCletus <smartchibuike2019@gmail.com>
2023-03-22 14:27:25 -0700

    task3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+/**
+ * op_add - Function adds integers
+ * @a: integer parameter
+ * @b: integer parameter
+ * Return: sum of both parameters
+ * Date: 22-03-2023
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - function substracts integers
+ * @a: int parameter
+ * @b: int parameter
+ * Return: difference between int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function multiplies two integers
+ * @a: int parameter
+ * @b: int parameter
+ * Return: product of int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - function divides two integers
+ * @a: int parameter
+ * @b: int parameter
+ * Return: result of division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - function returns modulus of int
+ * @a: int parameter
+ * @b: int parameter
+ * Return:remainder of division
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
