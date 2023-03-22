e05718487e5e6d667db1de1f5b44fa88fe5a9bf9
ConquerorCletus <smartchibuike2019@gmail.com>
2023-03-22 05:28:21 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - function that prints a name
+ * @name:The name
+ * @f: function pointer
+ * Return: void
+ * Date: 22-03-2023
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
