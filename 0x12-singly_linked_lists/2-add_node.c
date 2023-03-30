4790bea295076f9939a254c52d9edbb23557bcde
ConquerorCletus <smartchibuike2019@gmail.com>
2023-03-30 08:24:59 -0700

    2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * add_node - adds a new node to single linked list
+ * @head: First node of linked list.
+ * @str: string to store in the list.
+ * Return: address of new element.
+ * Date: 30-03-2023
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newElement;
+	size_t index;
+
+	newElement = malloc(sizeof(list_t));
+	if (newElement == NULL)
+		return (NULL);
+
+	newElement->str = strdup(str);
+
+	for (index = 0; str[index]; index++)
+		;
+
+	newElement->len = nchar;
+	newElement->next = *head;
+	*head = newElement;
+
+	return (*head);
+}
