d1c292594ab550ef67afbbcec7cab93730f9cac9
ConquerorCletus <smartchibuike2019@gmail.com>
2023-04-03 13:18:04 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * listint_len - this function prints number of the element.
+ * @h: pointer to new node.
+ * Return: Pointer to Node.
+ * Date: 03-04-2023
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t index = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		index++;
+	}
+	return (index);
+}
+
