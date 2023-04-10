dd33b0ea434dc4a328efbb2d64d950d7ea16962f
ConquerorCletus <smartchibuike2019@gmail.com>
2023-04-10 09:40:23 -0700

    advance  task

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * get_endianness - A function that indicates endians
+ * Return: 0 for big & 1 for little endian
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *endian = (endianhar *)&i;
+
+	if (*endian)
+		return (1);
+	else
+		return (0);
+}
