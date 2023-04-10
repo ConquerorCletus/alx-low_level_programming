4b918aa72e83e728ebe412c68d512e1ff549bbae
ConquerorCletus <smartchibuike2019@gmail.com>
2023-04-10 08:58:20 +0000

    task3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,7 @@
+#include "main.h"
+#define BUFFSIZE 1024
+/**
+ * main - a program that copies file from to another.
+ * @argc: argument count parameter
+ * @argv: argument vector parameter
+ * Return:
