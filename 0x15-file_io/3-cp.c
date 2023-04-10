2d61d5fb796989a18400c6c3f1cad6f1182fe9ec
ConquerorCletus <smartchibuike2019@gmail.com>
2023-04-10 04:23:05 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,68 +1,70 @@
 #include "main.h"
+/**
+ * checkError - checks if files can be opened.
+ * @file_from: source of copied file.
+ * @file_to: destination of copied file.
+ * @argv: arguments vector.
+ * Return: no return.
+ */
+void checkError(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
 
 /**
- * main - copies the content of a file to another file
- * @argc: arguement c
- * @argv: arguement v
- * Return: 0 on success
+ * main - program that copies from one file to another.
+ * @argc: Arguments count parameter.
+ * @argv: arguments vector parameter.
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
+	int file_from, file_to, close_file;
+	ssize_t charByte, writeByte;
+	char buf[1024];
+
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 
-	copy_file(argv[1], argv[2]);
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	checkError(file_from, file_to, argv);
 
+	charByte = 1024;
+	while (charByte == 1024)
+	{
+		charByte = read(file_from, buf, 1024);
+		if (charByte == -1)
+			checkError(-1, 0, argv);
+		writeByte = write(file_to, buf, charByte);
+		if (writeByte == -1)
+			error_file(0, -1, argv);
+	}
+
+	close_file = close(file_from);
+	if (close_file == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	close_file = close(file_to);
+	if (close_file == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
 	return (0);
 }
-
-/**
- * copy_file - copies the content of a file to another file
- * @file_from: source file name
- * @file_to: destination file name
- */
-void copy_file(char *file_from, char *file_to)
-{
-	int fd_from, fd_to;
-	ssize_t read_len, write_len;
-	char buffer[1024];
-
-	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 00664);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
-
-	while ((read_len = read(fd_from, buffer, 1024)) != 0)
-	{
-		write_len = write(fd_to, buffer, read_len);
-		if (write_len != read_len)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-			exit(99);
-		}
-	}
-
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
-}
-
